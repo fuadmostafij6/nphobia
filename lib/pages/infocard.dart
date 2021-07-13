bc5df4cae24a136e97fd187e323c41a8836b5417
webmostafij <fuadmostfij4@gmail.com>
2021-07-13 12:08:55 +0600

    first App

---
+++ lib/pages/infocard.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class InfoCard extends StatelessWidget {
+  // the values we need
+  final String text;
+  final IconData icon;
+  Function onPressed;
+
+  InfoCard(
+      {@required this.text, @required this.icon, @required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Card(
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+        child: ListTile(
+          leading: Icon(
+            icon,
+            color: Colors.teal,
+          ),
+          title: Text(
+            text,
+            style: TextStyle(
+                color: Colors.teal,
+                fontSize: 20,
+                fontFamily: "Source Sans Pro"),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsphobia/pages/infocard.dart';
 
 class User extends StatefulWidget {
   @override
@@ -8,8 +9,65 @@ class User extends StatefulWidget {
 class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('User')
-    );
+    return Container(child: Profile());
+  }
+}
+
+const url = "fuadmostafij.me";
+const email = "fuadmostafij3@gmail.com";
+const phone = "01798172012"; // not real number :)
+const location = "Dhaka, Bangladsh";
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey[800],
+        body: SafeArea(
+          minimum: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/user.png'),
+              ),
+              Text(
+                "Shivansh Singh",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blueGrey[200],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Source Sans Pro"),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+
+              // we will be creating a new widget name info carrd
+
+              InfoCard(text: phone, icon: Icons.phone, onPressed: () async {}),
+              InfoCard(text: url, icon: Icons.web, onPressed: () async {}),
+              InfoCard(
+                  text: location,
+                  icon: Icons.location_city,
+                  onPressed: () async {}),
+              InfoCard(text: email, icon: Icons.email, onPressed: () async {}),
+            ],
+          ),
+        ));
   }
 }
